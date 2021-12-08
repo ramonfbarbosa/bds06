cb2759f801cb7e9d625faf803b19ce8237e8b9bb
Ramon Barbosa <ramonmfb777@gmail.com>
2021-12-08 15:37:48 -0300

    Resources & Tests passed

--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,8 +1,9 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +18,8 @@ public class GenreService {
 	private GenreRepository repository;	
 	
 	@Transactional(readOnly = true)
-	public Page<GenreDTO> findAllPaged(Long genreId, Pageable pageable){
-		Page<Genre> page = repository.findAll(pageable);
-		return page.map(x -> new GenreDTO(x));
+	public List<GenreDTO> findAll(){
+		List<Genre> list = repository.findAll();
+		return list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());		
 	}
 }
--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.services;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,29 +15,31 @@ import com.devsuperior.movieflix.repositories.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService{
-
-	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserRepository repository;
 	
 	@Autowired
 	private AuthService authService;
-	
+		
 	@Transactional(readOnly = true)
-	public UserDTO dataAutenticatedUser() {
+	public UserDTO getLoggedUser() {
 		User user = authService.authenticated();
+		
 		return new UserDTO(user);
 	}
 	
 	@Override
+	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = repository.findByEmail(username);
-		if (user == null) {
-			logger.error("User not found: " + username);
-			throw new UsernameNotFoundException("Email not found");
+		
+		if(user == null) {
+			logger.error("Username "+username+" not found.");
+			throw new UsernameNotFoundException("Username "+username+" not found.");
 		}
-		logger.info("User found: " + username);
+		
 		return user;
 	}
 }
