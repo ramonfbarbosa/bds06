fdcd84d5cd1cef4a0501860e90033cf2dfdcfa8b
Ramon Barbosa <ramonmfb777@gmail.com>
2021-12-08 15:17:42 -0300

    Service ORM

--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -3,12 +3,14 @@ package com.devsuperior.movieflix.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.repositories.GenreRepository;
 
+@Service
 public class GenreService {
 
 	@Autowired
--- src/main/java/com/devsuperior/movieflix/services/ReviewService.java
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.ReviewDTO;
@@ -8,6 +9,7 @@ import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.Review;
 import com.devsuperior.movieflix.repositories.ReviewRepository;
 
+@Service
 public class ReviewService {
 	
 	@Autowired
